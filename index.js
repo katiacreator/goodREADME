1ed2e5b5ba2735bb4c6c8e1bf5563b6736c0b785
Katia Gibson <kcygibson@gmail.com>
2020-02-25 20:41:44 -0500

    fleshed out rough draft of prompts to be coded

--- index.js
+++ index.js
@@ -1,12 +1,57 @@
-const questions = [
+var inquirer = require("inquirer");
+var fs = require("fs");
 
+inquirer.prompt([
+  {
+    name: "Project Title",
+    type: "input",
+    message: "What is your project title?"
+  },
+  {
+    name: "Description",
+    type: "input",
+    message: "Please describe this project in 1-2 sentences."
+  },
+  {
+    name: "Table of Contents",
+    type: "list",
+    message: "Please select headings to be included in your table of contents."
+  },
+  {
+    name: "Installation",
+    type: "input",
+    message: "how to install this project?"
+  },
+  {
+    name: "Usage",
+    type: "",
+    message: ""
+  },
+  {
+    name: "License",
+    type: "checkbox",
+    message: "Please select a license type for this project"
+  },
+  {
+    name: "Contributions",
+    type: "input",
+    message:
+      "Please insert the first and last name of all contributors on this project"
+  },
+  {
+    name: "Tests",
+    type: "",
+    message: ""
+  }
+]);
+
+const questions = [
+  "Please insert an img link to your github profil pic",
+  "Please insert your github username"
 ];
 
-function writeToFile(fileName, data) {
-}
+function writeToFile(fileName, data) {}
 
-function init() {
-
-}
+function init() {}
 
 init();
