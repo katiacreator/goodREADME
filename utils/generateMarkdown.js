a7be5ee2a66768c23831c21c791bdd6bed13e9b1
Katia Gibson <kcygibson@gmail.com>
2020-02-26 21:48:43 -0500

    other than profile pic placement and badge this is done

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,24 +1,24 @@
 function generateMarkdown(data) {
   return `
 # ${data.ProjectTitle}
-## [Link] (${data.deployedUrl})
+### [(${data.deployedUrl})]${data.deployedUrl}
 ---
-# What Does This Project Do?
-## ${data.Description}
-# Installation Info to Run This Project
-## Enter ${data.Dependencies} in command line
-# How to Run Tests For This Project?
-## Enter ${data.Tests} in command line
-# Tips For Using This Project
-## ${data.Usage}
-# Project Demo
-# ![Image](${data.imageUrl})
-# ${data.altImage}
-# License(s)
-## ${data.License}
-# Contributions to this Project
-## ${data.Contributions}
-# [https://img.shields.io/badge/Version-v1.0-blue]
+## What Does This Project Do?
+### ${data.Description}
+## Installation Info to Run This Project
+### Enter ${data.Dependencies} in command line
+## How to Run Tests For This Project?
+### Enter ${data.Tests} in command line
+## Tips For Using This Project
+### ${data.Usage}
+## Project Demo
+![Image](${data.imageUrl})
+### ${data.altImage}
+## License(s)
+### ${data.License}
+## Contributions to this Project
+### ${data.Contributions}
+####[https://img.shields.io/badge/Version-v1.0-blue]
 `;
 }
 
